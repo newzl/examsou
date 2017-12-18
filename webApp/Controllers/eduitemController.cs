3a199e964f245864c1eef261a68315e6392995d6
hllive <hllive@163.com>
2017-12-18 17:38:04 +0800

    201711218_hyl

---
+++ webApp/Controllers/eduitemController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace webApp.Controllers
+{
+    public class eduitemController : Controller
+    {
+        // GET: eduitem
+        public ActionResult search()
+        {
+            return View();
+        }
+        public ActionResult detail() {
+            return View();
+        }
+    }
+}
